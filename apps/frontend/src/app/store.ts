d2bedce68e6ac326267a1245fab243cebc056c83
Trevor Wilson <wilsontr@gmail.com>
2024-02-05 12:27:28 -0700

    Fix tsconfig aliases in vite

--- apps/frontend/src/app/store.ts
+++ apps/frontend/src/app/store.ts
@@ -1,7 +1,7 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-import { emptySplitApi, filmsApi } from "@arbus/rtk-api"
+import { emptySplitApi } from "@arbus/rtk-api"
 
 
 // `combineSlices` automatically combines the reducers using
