6734c7fe9706f20f491bb89166a23b8d722258bb
Trevor Wilson <wilsontr@gmail.com>
2024-02-07 16:13:47 -0700

    Add routes and router for UI pages

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,39 +1,63 @@
-import { SyntheticEvent, useCallback, useState } from "react";
+import { useCallback, useState } from "react";
+import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+
 import "./App.css";
 
-import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Box, Tab } from "@mui/material";
-import { AppTab } from "./App.model";
+import { Box, Tab, Tabs } from "@mui/material";
 import { Films } from "./features/films";
 import { Developers } from "./features/developers";
 
 const App = () => {
-  const [currentTab, setCurrentTab] = useState<AppTab>(AppTab.Developers);
+  const [currentTab, setCurrentTab] = useState<string>("/films");
   const handleTabChange = useCallback(
-    (event: SyntheticEvent, newValue: AppTab) => {
-      setCurrentTab(newValue);
+    (_: any, newTab: string) => {
+      setCurrentTab(newTab);
     },
-    [],
+    [setCurrentTab],
   );
   return (
-    <div className="App">
-      <header className="App-header">
-        <TabContext value={currentTab}>
-          <Box>
-            <TabList onChange={handleTabChange}>
-              <Tab label="Films" value={AppTab.Films} />
-              <Tab label="Developers" value={AppTab.Developers} />
-            </TabList>
-          </Box>
-          <TabPanel value={AppTab.Films}>
-            <Films />
-          </TabPanel>
-          <TabPanel value={AppTab.Developers}>
-            <Developers />
-          </TabPanel>
-        </TabContext>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <>
+            <Box>
+              <Tabs value={currentTab} onChange={handleTabChange}>
+                <Tab
+                  label="Films"
+                  value="/films"
+                  component={Link}
+                  to="/films"
+                />
+                <Tab
+                  label="Developers"
+                  value="/developers"
+                  component={Link}
+                  to="/developers"
+                />
+              </Tabs>
+            </Box>
+            <Routes>
+              <Route
+                path="/films"
+                element={
+                  <Box>
+                    <Films />
+                  </Box>
+                }
+              />
+              <Route
+                path="/developers"
+                element={
+                  <Box>
+                    <Developers />
+                  </Box>
+                }
+              />
+            </Routes>
+          </>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 };
 
